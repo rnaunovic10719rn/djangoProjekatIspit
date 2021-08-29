7bd214e9f2ec8faf4e02605276a86f8f68089695
Rastko <rnaunovic10719rn@raf.rs>
2021-08-29 21:50:48 +0200

    90%

--- djangoProjekat2/tastatura/urls.py
+++ djangoProjekat2/tastatura/urls.py
@@ -7,5 +7,6 @@ app_name = 'tastatura'
 urlpatterns = [
     path('', views.dash, name='home'),
     path('proizvodjac', views.proizvodjac, name='proizvodjac'),
-    path('tastatura', views.tastatura, name='tastatura')
+    path('tastatura', views.tastatura, name='tastatura'),
+    path('user', views.user, name='user'),
 ]
--- djangoProjekat2/tastatura/views.py
+++ djangoProjekat2/tastatura/views.py
@@ -8,7 +8,9 @@ def dash(request):
     tastatura = Tastatura.objects.all()
     total_proizvodjac = proizvodjac.count()
     total_tastatura = tastatura.count()
-    return render(request, 'tastatura/dashboard.html', {'proizvodjac': proizvodjac, 'tastatura': tastatura, 'total_proizvodjac': total_proizvodjac, 'total_tastatura' : total_tastatura})
+    user = User.objects.all()
+    total_user = user.count()
+    return render(request, 'tastatura/dashboard.html', {'proizvodjac': proizvodjac, 'tastatura': tastatura, 'total_proizvodjac': total_proizvodjac, 'total_tastatura' : total_tastatura, 'user': user, 'total_user': total_user})
 
 def proizvodjac(request):
     proizvodjac = Proizvodjac.objects.all()
@@ -19,3 +21,8 @@ def tastatura(request):
     tastatura = Tastatura.objects.all()
     total_tastatura = tastatura.count()
     return render(request, 'tastatura/tastatura.html', {'tastatura': tastatura, 'total_tastatura': total_tastatura})
+
+def user(request):
+    user = User.objects.all()
+    total_user = user.count()
+    return render(request, 'tastatura/user.html', {'user': user, 'total_user': total_user})

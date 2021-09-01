aefc9edf872567fdcf2f15b3bbf7d6b7a9eef89f
Rastko <rnaunovic10719rn@raf.rs>
2021-09-01 17:14:37 +0200

    100%

--- djangoProjekat2/tastatura/urls.py
+++ djangoProjekat2/tastatura/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'tastatura'
 
 urlpatterns = [
-    path('', views.dash, name='home'),
+    path('', views.tastatura, name='home'),
     path('proizvodjac', views.proizvodjac, name='proizvodjac'),
     path('tastatura', views.tastatura, name='tastatura'),
     path('user', views.user, name='user'),
